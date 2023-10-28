622c3c817a0b44c9d789711c99f808b8278b869d
Canius-p <boiinventer@gmail.com>
2023-10-28 19:59:32 +0545

    add data using create blog

--- frontend/src/pages/CreateBlog/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog/CreateBlog.jsx
@@ -1,31 +1,56 @@
+import axios from "axios"
 import Navbar from "../../Components/Navbar/Navbar"
+import { useState} from "react"
+import { useNavigate } from "react-router-dom"
+const CreateBlog = () => {
+    const navigate = useNavigate()
+    const [title,setTitle] = useState("")
+    const [subTitle, setSubTitle] = useState("")
+    const [description, setDescription] = useState("")
 
-function CreateBlog() {
+    const CreateBlog =async(e)=>{
+        e.preventDefault()
+        const data = {
+            //fetching data from backend
+            title:title,
+            subTitle:subTitle,
+            description:description
+        }
+        //sending data to api
+     const response =  await axios.post("http://localhost:2000/blogs",data)    
+if(response.status == 201){
+    alert("Blog created successfully")
+    navigate("/")
+}else{
+    alert("something went wrong")
+}
+
+} 
   return (
     <>
     <Navbar/>
 <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
       <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new Blog</h2>
-      <htmlForm action="#">
+      <form onSubmit={CreateBlog}>
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
               <div className="sm:col-span-2">
-                  <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title </label>
-                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Title" required="" />
+                  <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title </label>
+                  <input type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type Title" required="" onChange={(e)=>setTitle(e.target.value)}/>
               </div>
               <div className="w-full">
                   <label htmlFor="subTitle" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sub Title</label>
-                  <input type="text" name="subTitle" id="subTitle" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder=" Type subTitle" required=""/>
+                  <input type="text" name="subTitle" id="subTitle" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder=" Type subTitle" required=""onChange={(e)=>setSubTitle(e.target.value)}/>
               </div>
               <div className="sm:col-span-2">
                   <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                  <textarea id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
+                  <textarea id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here" onChange={(e)=>setDescription(e.target.value)}></textarea>
               </div>
           </div>
           <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-              Add product
+              Add 
           </button>
-      </htmlForm>
+      </form>
   </div>
 </section> 
 </>
